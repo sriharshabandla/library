d5cdec92eb42169195b8df86e33d7a408c121629
sriharshabandla <sriharshabandla@gmail.com>
2019-10-10 17:51:14 +0530

    library project

---
+++ libraryapp/libapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Book(models.Model):
+    book_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.book_name
